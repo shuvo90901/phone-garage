fb4b72e125bc26df085b90c22dc06260930a2258
shuvo90901 <shuvo90901>
2022-11-24 23:50:59 +0600

    extra section added

--- src/Pages/Home/ExtraSection/ExtraSection.js
+++ src/Pages/Home/ExtraSection/ExtraSection.js
@@ -4,7 +4,13 @@ import img from '../../../images/secondhand.jpg'
 const ExtraSection = () => {
     return (
         <div className='flex justify-evenly items-center my-40'>
-
+            <div className='w-1/3'>
+                <img className='rounded-lg' src={img} alt="" />
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold mb-10"><i>Buy and Sell Secondhand Phone</i></h2>
+                <p className="text-xl">You can buy any mobile phone in low price.<br />Also you can Sell mobile phone with well profit.</p>
+            </div>
         </div>
     );
 };
